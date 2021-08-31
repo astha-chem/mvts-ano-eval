beaddc29c3ca36284ab77e3963a5d3f8072590a8
Astha Garg <astha.garg@chordx.co>
2021-08-31 21:46:29 +0800

    updated setup

--- setup.py
+++ setup.py
@@ -2,14 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='mvts-eval',
-    author='Astha Garg, Jules Samaran, Wenyu Zhang',
+    author='Astha Garg, Wenyu Zhang, Jules Samaran',
     description='An evaluation of multivariate time-series anomaly detection algorithms',
     long_description=open('README.md').read(),
     version='0.0',
     packages=find_packages(),
     scripts=[],
-    # Requirements for executing the project (not development)
-    # install_requires=parse_requirements('requirements.txt'),
-    # url='github.com/KDD-OpenSource/DeepADoTS',
-    # license='MIT License',
 )
